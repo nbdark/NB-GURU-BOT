654ef4fa66655bd7855fe670e02a8e22880a264a
nbdark <127912609+nbdark@users.noreply.github.com>
2023-09-16 22:28:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94704452797', 'me', true],
+  ['94704452797', 'Alpha Dark Bot', true], 
   [''] 
 ] //Number of owners
 
